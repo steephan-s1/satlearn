1e9dbadc51aca6d9469c60bad5401187e41b02fb
steephan-s1 <58932946+steephan-s1@users.noreply.github.com>
2021-09-21 11:38:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,25 +16,35 @@ resource "aws_iam_policy_attachment" "TransferLogPolicy" {
 
 resource "aws_iam_policy_attachment" "Lambdaexecutionpolicy" {
   name       = "Lambdaexecutionrole"
-  roles      = ["${aws_iam_role.LambdaExcutionRole.name}"]
-  policy_arn = "${aws_iam_policy.LambadaBasicExecutionPolicy.arn}"
+  roles      = ["${aws_iam_role.Lambdaexecutionpolicy.name}"]
+  policy_arn = "${aws_iam_policy.LambdaBasicExecutionPolicy.arn}"
+}
+
+resource "aws_iam_policy_attachment" "Lambdaexecutionpolicy1" {
+  name       = "Lambdaexecutionrole"
+  roles      = ["${aws_iam_role.Lambdaexecutionpolicy.name}"]
   policy_arn = "${aws_iam_policy.LambdaSecretsPolicy.arn}"
+}
+
+resource "aws_iam_policy_attachment" "TransferCanReadApi1" {
+  name       = "TransferCanReadApi"
+  roles      = ["${aws_iam_role.TransferIdentityProviderRole.name}"]
+  policy_arn = "${aws_iam_policy.TransferCanReadThisApi.arn}"
 }
 
 resource "aws_iam_policy_attachment" "TransferCanReadApi" {
   name       = "TransferCanReadApi"
   roles      = ["${aws_iam_role.TransferIdentityProviderRole.name}"]
   policy_arn = "${aws_iam_policy.TransferCanInvokeThisApi.arn}"
-  policy_arn = "${aws_iam_policy.TransferCanReadThisApi.arn}"
 }
 
-resource "aws_lambda_function" "GetUserConfigLambda" {
+resource "aws_lambda_function" "GetUserConfigLambda1" {
   role             = "${aws_iam_role.Lambdaexecutionpolicy.arn}"
   handler          = "lambda.handler"
   runtime          = "python3.7"
   filename         = "lambda.zip"
   function_name    = "GetUserConfigLambda"
-  source_code_hash = "${file("index.py")}"
+source_code_hash = filebase64sha256("lambda.zip")
 }
 
 resource "aws_api_gateway_rest_api" "example" {
@@ -48,7 +58,6 @@ resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
   parent_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
   path_part = "{server}"
-  path   = "servers/{serverId}/users/username/config"
 }
 resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
@@ -64,26 +73,16 @@ resource "aws_api_gateway_integration" "lambda" {
 
   integration_http_method = "GET"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.GetUserConfigLambda.invoke_arn}"
+  uri                     = "${aws_lambda_function.GetUserConfigLambda1.invoke_arn}"
 }
 
 
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.GetUserConfigLambda.function_name}"
+  function_name = "${aws_lambda_function.GetUserConfigLambda1.function_name}"
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:ap-south-1:547155362487:${aws_api_gateway_rest_api.example.id}/*/${aws_api_gateway_method.proxy.http_method}${aws_api_gateway_resource.proxy.path}"
-}
-
-
-resource "aws_lambda_function" "GetUserConfigLambda" {
-  role             = "arn:aws:iam::547155362487:role/demo-LambdaExecutionRole-FG713KFMD0ST"
-  handler          = "lambda.handler"
-  runtime          = "python3.7"
-  filename         = "lambda.zip"
-  function_name    = "GetUserConfigLambda"
-  source_code_hash = filebase64sha256("lambda.zip")
 }
